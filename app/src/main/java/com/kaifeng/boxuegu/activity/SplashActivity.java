8af69329a2ffa9054697f9a2779ad7d63bf706ac
huangkaifeng <1195745286@qq.com>
2018-01-06 23:12:20 +0800

    资源文件的上传及其部分结构的构建

--- app/src/main/java/com/kaifeng/boxuegu/activity/SplashActivity.java
+++ app/src/main/java/com/kaifeng/boxuegu/activity/SplashActivity.java
@@ -1,13 +1,15 @@
-package com.kaifeng.boxuegu;
+package com.kaifeng.boxuegu.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.kaifeng.boxuegu.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
