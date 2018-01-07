72baab1974c191fae11bb881aed3b61bd5cdb73a
huangkaifeng <1195745286@qq.com>
2018-01-07 12:13:55 +0800

    资源布局代码部分及其常量类构建

---
+++ app/src/main/java/com/kaifeng/boxuegu/bean/CourseBean.java
@@ -0,0 +1,13 @@
+package com.kaifeng.boxuegu.bean;
+
+/**
+ * Created by Administrator on 2018/1/7.
+ */
+
+public class CourseBean {
+    public int id;
+    public String imgTitle;
+    public String title;
+    public String intro;
+    public String icon;
+}
---
+++ app/src/main/java/com/kaifeng/boxuegu/bean/ExercisesBean.java
@@ -0,0 +1,20 @@
+package com.kaifeng.boxuegu.bean;
+
+/**
+ * Created by Administrator on 2018/1/7.
+ */
+
+public class ExercisesBean {
+    public int id;
+    public String title;
+    public String content;
+    public int background;
+    public int subjectId;
+    public String subject;
+    public String a;
+    public String b;
+    public String c;
+    public String d;
+    public int answer;
+    public int select;
+}
