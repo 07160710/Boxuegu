160925c1e55e46b004e87bb124afbaf337d918bf
huangkaifeng <1195745286@qq.com>
2018-01-13 22:36:00 +0800

    part of java file upload

---
+++ app/src/main/java/com/kaifeng/boxuegu/bean/UserBean.java
@@ -0,0 +1,16 @@
+package com.kaifeng.boxuegu.bean;
+
+/**
+ * Created by Administrator on 2018/1/13.
+ */
+
+public class UserBean {
+    //用户名
+    public String userName;
+    //昵称
+    public String nickName;
+    //性别
+    public String sex;
+    //签名
+    public String signature;
+}
---
+++ app/src/main/java/com/kaifeng/boxuegu/bean/VideoBean.java
@@ -0,0 +1,13 @@
+package com.kaifeng.boxuegu.bean;
+
+/**
+ * Created by Administrator on 2018/1/13.
+ */
+
+public class VideoBean {
+    public int chapterId;
+    public int videoId;
+    public String title;
+    public String secondTitle;
+    public String videoPath;
+}
