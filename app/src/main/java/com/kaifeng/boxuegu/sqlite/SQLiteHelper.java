49111dae3adfc946695a0f719f00b55551ad334d
huangkaifeng <1195745286@qq.com>
2018-01-17 22:48:52 +0800

    upload SQLiteHelper java file

--- app/src/main/java/com/kaifeng/boxuegu/sqlite/SQLiteHelper.java
+++ app/src/main/java/com/kaifeng/boxuegu/sqlite/SQLiteHelper.java
@@ -1,8 +1,48 @@
 package com.kaifeng.boxuegu.sqlite;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Administrator on 2018/1/15.
  */
 
-public class SQLiteHelper {
+public class SQLiteHelper extends SQLiteOpenHelper{
+    private static final int DB_VERSION = 1;
+    public static String DB_NAME = "bxg.db";
+    public static final String U_USERINFO = "userinfo";
+    public static final String U_VIDEO_PLAY_LIST = "videoplaylist";
+    public SQLiteHelper(Context context){
+        super(context,DB_NAME,null,DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //个人信息
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + U_USERINFO + "( "
+         + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+         + "userName VARCHAR, "
+         + "nickName VARCHAR, "
+         + "sex VARCHAR, "
+         + "signature VARCHAR"
+         + ")");
+        //播放记录表
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + U_VIDEO_PLAY_LIST+ "( "
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "userName VARCHAR, "
+                + "chapterId INT, "
+                + "videoId INT, "
+                + "videoPath VARCHAR, "
+                + "title VARCHAR,"
+                + "secondTitle VARCHAR"
+                + ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + U_USERINFO);
+        db.execSQL("DROP TABLE IF EXISTS " + U_VIDEO_PLAY_LIST);
+        onCreate(db);
+    }
 }
