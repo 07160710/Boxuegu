1577727da53e84943046d2b719f06112b65cdcb2
huangkaifeng <1195745286@qq.com>
2018-01-27 22:43:53 +0800

    VideoPlay is OK and ChangeUserInfo complete almost 。。。

--- app/src/main/java/com/kaifeng/boxuegu/activity/ChangeUserInfoActivity.java
+++ app/src/main/java/com/kaifeng/boxuegu/activity/ChangeUserInfoActivity.java
@@ -1,15 +1,60 @@
 package com.kaifeng.boxuegu.activity;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.kaifeng.boxuegu.R;
 
 public class ChangeUserInfoActivity extends AppCompatActivity {
+    private TextView tv_main_title,tv_back;
+    private RelativeLayout rl_title_bar;
+    private TextView tv_save;
+    private String title,content;
+    private int flag;
+    private EditText et_content;
+    private ImageView iv_delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_user_info);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+    }
+    private void init(){
+        title = getIntent().getStringExtra("title");
+        content = getIntent().getStringExtra("content");
+        flag = getIntent().getIntExtra("flag", 0);
+        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText(title);
+        rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
+        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
+        tv_back = (TextView) findViewById(R.id.tv_back);
+        tv_save = (TextView) findViewById(R.id.tv_save);
+        tv_save.setVisibility(View.VISIBLE);
+        et_content = (EditText) findViewById(R.id.et_content);
+        iv_delete = (ImageView) findViewById(R.id.iv_delete);
+        if (!TextUtils.isEmpty(content)){
+            et_content.setText(content);
+            et_content.setSelection(content.length());
+        }
+        contentListener();
+        tv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangeUserInfoActivity.this.finish();
+            }
+        });
+    }
+    private void contentListener(){
+
     }
 }
