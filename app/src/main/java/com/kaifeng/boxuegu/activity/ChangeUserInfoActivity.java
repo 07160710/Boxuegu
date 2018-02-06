19d32775938b34324d09f633bd1a8849b4a6c5f1
huangkaifeng <1195745286@qq.com>
2018-02-06 12:12:24 +0800

    after holiday upload again

--- app/src/main/java/com/kaifeng/boxuegu/activity/ChangeUserInfoActivity.java
+++ app/src/main/java/com/kaifeng/boxuegu/activity/ChangeUserInfoActivity.java
@@ -1,15 +1,20 @@
 package com.kaifeng.boxuegu.activity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Selection;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kaifeng.boxuegu.R;
 
@@ -53,8 +58,92 @@ public class ChangeUserInfoActivity extends AppCompatActivity {
                 ChangeUserInfoActivity.this.finish();
             }
         });
+        iv_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et_content.setText("");
+            }
+        });
+        tv_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                String etContent = et_content.getText().toString().trim();
+                switch (flag){
+                    case 1:
+                        if (!TextUtils.isEmpty(etContent)){
+                            data.putExtra("nickName",etContent);
+                            setResult(RESULT_OK,data);
+                            Toast.makeText(ChangeUserInfoActivity.this,"保存成功",Toast.LENGTH_SHORT).show();
+                            ChangeUserInfoActivity.this.finish();
+                        }else {
+                            Toast.makeText(ChangeUserInfoActivity.this,"昵称不能为空",Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 2:
+                        if (!TextUtils.isEmpty(etContent)){
+                            data.putExtra("signature",etContent);
+                            setResult(RESULT_OK,data);
+                            Toast.makeText(ChangeUserInfoActivity.this,"保存成功",Toast.LENGTH_SHORT).show();
+                            ChangeUserInfoActivity.this.finish();
+                        }else {
+                            Toast.makeText(ChangeUserInfoActivity.this,"签名不能为空",Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                }
+            }
+        });
     }
     private void contentListener(){
-
+        et_content.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Editable editable = et_content.getText();
+                int len = editable.length();
+                if (len > 0){
+                    iv_delete.setVisibility(View.VISIBLE);
+                }else{
+                    iv_delete.setVisibility(View.GONE);
+                }
+                switch (flag){
+                    case 1:
+                        if (len > 8){
+                            int selEndIndex = Selection.getSelectionEnd(editable);
+                            String str = editable.toString();
+                            String newStr = str.substring(0,8);
+                            et_content.setText(newStr);
+                            editable = et_content.getText();
+                            int newLen = editable.length();
+                            if (selEndIndex > newLen){
+                                selEndIndex = editable.length();
+                            }
+                            Selection.setSelection(editable,selEndIndex);
+                        }
+                        break;
+                    case 2:
+                        if (len > 16){
+                            int selEndIndex = Selection.getSelectionEnd(editable);
+                            String str = editable.toString();
+                            String newStr = str.substring(0,16);
+                            et_content.setText(newStr);
+                            editable = et_content.getText();
+                            int newLen = editable.length();
+                            if (selEndIndex > newLen){
+                                selEndIndex = editable.length();
+                            }
+                            Selection.setSelection(editable,selEndIndex);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
 }
